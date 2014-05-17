4d04186790311926f94e7e144482e26c48815ce6
Melody <mgk1068@violin.local>
2014-05-17 01:50:05 -0400

    Added "popover"
    Popovers work in iPad, but they're different in iPhone? still looking
    into it. Right now, it fills the whole view :/ it's also not centered
    for some reason. Displays some information - need to add

---
+++ RITMidnightOil_iPhone/RITMidnightOil_iPhone/ItemDetailVC.m
@@ -0,0 +1,49 @@
+//
+//  ItemDetailVC.m
+//  RITMidnightOil_iPhone
+//
+//  Created by Melody Kelly on 5/16/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import "ItemDetailVC.h"
+
+@interface ItemDetailVC ()
+
+@end
+
+@implementation ItemDetailVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ RITMidnightOil_iPhone/RITMidnightOil_iPhone/ItemPopoverBackground.h
@@ -0,0 +1,13 @@
+//
+//  ItemPopoverBackground.h
+//  RITMidnightOil_iPhone
+//
+//  Created by Melody Kelly on 5/16/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemPopoverBackground : UIPopoverBackgroundView
+
+@end
