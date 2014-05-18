da1e8470587334742126ecf4b1bfc663a52c93d9
Melody <mgk1068@violin.local>
2014-05-18 12:49:47 -0400

    Added order class

---
+++ RITMidnightOil_iPhone/RITMidnightOil_iPhone/Order.h
@@ -0,0 +1,13 @@
+//
+//  Order.h
+//  RITMidnightOil_iPhone
+//
+//  Created by Melody Kelly on 5/17/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Order : NSObject
+
+@end
---
+++ RITMidnightOil_iPhone/RITMidnightOil_iPhone/Order.m
@@ -0,0 +1,13 @@
+//
+//  Order.m
+//  RITMidnightOil_iPhone
+//
+//  Created by Melody Kelly on 5/17/14.
+//  Copyright (c) 2014 Student. All rights reserved.
+//
+
+#import "Order.h"
+
+@implementation Order
+
+@end
