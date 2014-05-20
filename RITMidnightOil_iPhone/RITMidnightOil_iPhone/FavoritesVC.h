98a5543c265735e07f32d51cf3f8bb1278f484d7
Julienne Ablay <jla1319@rit.edu>
2014-05-20 01:09:55 -0400

    added comments

--- RITMidnightOil_iPhone/RITMidnightOil_iPhone/FavoritesVC.h
+++ RITMidnightOil_iPhone/RITMidnightOil_iPhone/FavoritesVC.h
@@ -2,9 +2,12 @@
 //  FavoritesVC.h
 //  RITMidnightOil_iPhone
 //
-//  Created by Student on 5/18/14.
+//  Created by Julienne Ablay on 5/18/14.
 //  Copyright (c) 2014 Student. All rights reserved.
 //
+//  Summary: This View Controller controls the 'Favorites' tab in the app.
+//  It keeps track of favorited items so they can be more accessible.
+//  It is also easy to delete favorites from the table and from the itemDetailVC as well.
 
 #import <UIKit/UIKit.h>
 #import "Menu.h"
